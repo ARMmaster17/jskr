c4bd97c358056d646ec610ff04a22483e8e05ba3
Joshua Zenn <wordman05@gmail.com>
2021-10-13 08:59:23 -0400

    Created a MVC the right way

--- jskr/Data/ApplicationDbContext.cs
+++ jskr/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using jskr.Models;
 
 namespace jskr.Data
 {
@@ -12,5 +13,6 @@ namespace jskr.Data
             : base(options)
         {
         }
+        public DbSet<jskr.Models.Source> Source { get; set; }
     }
 }
