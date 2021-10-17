21a30f5789ed36e41a2ef55eb0f8e77dfee6afb2
Joshua Zenn <wordman05@gmail.com>
2021-10-17 09:56:12 -0400

    k8s deploy testing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,9 @@ if [ -f /app/tmp/pids/server.pid ]; then
   rm /app/tmp/pids/server.pid
 fi
 
+echo "==============="
+echo "|$RAILS_MASTER_KEY|"
+echo "==============="
+
 bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
 exec bundle exec "$@"
