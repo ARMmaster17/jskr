dfde78de100d70b3de1102f06214b0055ab9401a
Joshua Zenn <wordman05@gmail.com>
2021-10-20 19:54:50 -0400

    Local database for faster testing

---
+++ db/schema.rb
@@ -0,0 +1,77 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_10_17_203520) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.date "occurred"
+    t.integer "state_id", null: false
+    t.integer "lead_id", null: false
+    t.string "person"
+    t.text "notes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lead_id"], name: "index_events_on_lead_id"
+    t.index ["state_id"], name: "index_events_on_state_id"
+  end
+
+  create_table "leads", force: :cascade do |t|
+    t.string "company"
+    t.string "position"
+    t.integer "source_id", null: false
+    t.string "location"
+    t.text "notes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["source_id"], name: "index_leads_on_source_id"
+    t.index ["user_id"], name: "index_leads_on_user_id"
+  end
+
+  create_table "sources", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_sources_on_user_id"
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_states_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "events", "leads"
+  add_foreign_key "events", "states"
+  add_foreign_key "leads", "sources"
+  add_foreign_key "leads", "users"
+  add_foreign_key "sources", "users"
+  add_foreign_key "states", "users"
+end
