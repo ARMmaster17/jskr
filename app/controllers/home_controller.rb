17b14772bc63679a60d6717a581e8fa90304c160
Joshua Zenn <wordman05@gmail.com>
2021-10-28 22:07:12 -0400

    Card view and total event type breakdown

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if user_signed_in? then
       @timeline_events = current_user.events.all.joins(:state).group("states.name").group_by_day(:occurred, last: 21).count
-      puts @timeline_events
+      @current_state_breakdown = current_user.events.all.joins(:state).group("states.name").count
     end
   end
 end
