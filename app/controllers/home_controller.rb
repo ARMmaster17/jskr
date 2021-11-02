ff42359d627a91040cd93c896a781488fcf01d61
Joshua Zenn <wordman05@gmail.com>
2021-11-02 17:36:05 -0400

    #17 UCB algorithm and card on dashboard
    Closes #17

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
     if user_signed_in? then
       @timeline_events = current_user.events.all.joins(:state).group("states.name").group_by_day(:occurred, last: 21).count
       @current_state_breakdown = current_user.events.all.joins(:state).group("states.name").count
+      @next_source_rec = helpers.ucb_next_source
     end
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,23 @@
 module HomeHelper
+  def ucb_next_source
+    # UCB method to calculate next source recommendation.
+    ucb_sources = Hash.new
+    total_t = current_user.events.joins(lead: :source).where("sources.startable = ?", true).count
+    startable_sources = current_user.sources.where(startable: true)
+    startable_sources.each do |ssrc|
+      ssrc_leads = current_user.leads.where(source: ssrc)
+      sleads_score = 0
+      ssrc_leads.each do |slead|
+        if slead.events.count > 0
+          sleads_score += slead.events.order(occurred: :desc, id: :desc).first.state.score
+        end
+      end
+      sleads_mean_score = sleads_score.to_f / ssrc_leads.length.to_f
+      ucb_sources[ssrc.name] = sleads_mean_score + Math.sqrt((2 * Math.log(total_t)) / ssrc_leads.count)
+      if ucb_sources[ssrc.name].nil? or ucb_sources[ssrc.name].nan?
+        ucb_sources[ssrc.name] = 0
+      end
+    end
+    return ucb_sources.max_by{|k,v| v}[0]
+  end
 end
