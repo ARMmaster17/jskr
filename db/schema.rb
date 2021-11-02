ea53c50d36e0b2fce9faf6eea963d15fde265031
Joshua Zenn <wordman05@gmail.com>
2021-11-02 12:50:02 -0400

    #17 State scoring

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_030748) do
+ActiveRecord::Schema.define(version: 2021_11_02_162921) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_10_31_030748) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.integer "score", default: 0
     t.index ["user_id"], name: "index_states_on_user_id"
   end
 
